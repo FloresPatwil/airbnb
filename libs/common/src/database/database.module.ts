8626bb5eaab0f22c919285eb020873c8b1de29b1
FloresPatwil <pfloresri@unsa.edu.pe>
2024-04-09 20:05:06 -0500

    common library

---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '../config/config.module';
+
+@Module({
+    imports: [
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: (configService:ConfigService) => ({        //funcion de fabrica : para la conexion de mongodb
+                uri: configService.get('MONGODB_URI'),
+            }),
+            inject: [ConfigService],
+        })
+    ],//[MongooseModule.forRoot('mongodb://localhost:27017/airbnb')],
+})
+export class DatabaseModule {}
