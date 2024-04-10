b0c2e4a1ddf3ba049495d9c2e334f9864c7d9abc
FloresPatwil <pfloresri@unsa.edu.pe>
2024-04-09 22:32:58 -0500

    Implements schema Joi

--- libs/common/src/config/config.module.ts
+++ libs/common/src/config/config.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { ConfigService, 
     ConfigModule as NestConfigModule
 } from '@nestjs/config';
+import * as Joi from 'joi';  // Toda la configuracion de Joi
 
 @Module({
-    imports: [NestConfigModule.forRoot()],
+    imports: [NestConfigModule.forRoot({
+        validationSchema : Joi.object({     // Validar variable de entorno
+            MONGODB_URI: Joi.string().required(),   // Variable de entorno de tipo String
+            
+        })
+    })],
     providers: [ConfigService],
-    exports: [ConfigService],
+    exports: [ConfigService],   
 })
 export class ConfigModule {}
